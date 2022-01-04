217522608ed75c04854afd797f9cec26f2c95b90
Ansul Sah <schuylerdare96@yahoo.com>
2022-01-04 20:55:16 +0530

    added dummy meetup page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://images.unsplash.com/photo-1617943133078-3ad4f4411aeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dW5zcGFsc2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+      address='ewrwrwe'
+      title='first meetup'
+      description='meetup description'
+    />
+  );
+}
+
+export default MeetupDetails;
